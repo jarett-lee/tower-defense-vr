ec0adfde4b6a6e18c28f5c4e7966b4c3a40b6f6a
jarett-lee <jarett.lee.pi@gmail.com>
2018-11-14 18:15:18 -0500

    Add to sample scene

---
+++ Assets/Shared/Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    float _distanceToGoal;
+    public float DistanceToGoal {
+        get
+        {
+            return _distanceToGoal;
+        }
+    }
+
+	void Start () {
+        target = Waypoints.points[0];
+
+        Vector3 dir = target.position - transform.position;
+        _distanceToGoal = dir.magnitude;
+	}
+	
+	void Update () {
+        Vector3 dir = target.position - transform.position;
+        int waypointsLeft = Waypoints.points.Length - waypointIndex;
+        _distanceToGoal = 10000f * waypointsLeft + dir.magnitude;
+
+        Vector3 move = dir.normalized * speed * Time.deltaTime;
+        transform.Translate(move, Space.World);
+
+        if (dir.magnitude <= move.magnitude)
+        {
+            GetNextWaypoint();
+        }
+	}
+
+    void GetNextWaypoint()
+    {
+        if (waypointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        waypointIndex++;
+        target = Waypoints.points[waypointIndex];
+    }
+}
---
+++ Assets/Shared/Scripts/Turret.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+    private Transform target;
+    public float range = 15f;
+
+    public string enemyTag = "enemy";
+
+    private void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestEnemyDistanceToGoal = float.PositiveInfinity;
+        GameObject enemyClosestToGoal = null;
+
+        foreach(GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            bool inRange = distanceToEnemy < range;
+            float enemyDistanceToGoal = enemy.GetComponent<Enemy>().DistanceToGoal;
+            bool closer = shortestEnemyDistanceToGoal > enemyDistanceToGoal;
+            if (inRange && closer)
+            {
+                shortestEnemyDistanceToGoal = enemyDistanceToGoal;
+                enemyClosestToGoal = enemy;
+            }
+        }
+
+        if (enemyClosestToGoal != null)
+        {
+            target = enemyClosestToGoal.transform;
+            Debug.Log(shortestEnemyDistanceToGoal);
+        }
+    }
+
+    private void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        // TODO improve target locking script
+        transform.LookAt(target.position);
+    }
+}
