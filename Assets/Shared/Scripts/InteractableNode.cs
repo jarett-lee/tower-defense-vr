14e9529abd12098183f80b17209abf04cfd40bd5
jarett-lee <jarett.lee.pi@gmail.com>
2018-11-30 13:09:21 -0500

    Add node interaction framework

---
+++ Assets/Shared/Scripts/InteractableNode.cs
@@ -0,0 +1,58 @@
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+//
+// Purpose: Demonstrates how to create a simple interactable object
+//
+//=============================================================================
+
+using UnityEngine;
+using System.Collections;
+using Valve.VR.InteractionSystem;
+
+//-------------------------------------------------------------------------
+[RequireComponent(typeof(Interactable))]
+public class InteractableNode : MonoBehaviour
+{
+    private Interactable interactable;
+
+    //-------------------------------------------------
+    void Awake()
+    {
+        interactable = GetComponent<Interactable>();
+    }
+
+
+    //-------------------------------------------------
+    // Called when a Hand starts hovering over this object
+    //-------------------------------------------------
+    private void OnHandHoverBegin(Hand hand)
+    {
+        // Display turret above node
+    }
+
+
+    //-------------------------------------------------
+    // Called when a Hand stops hovering over this object
+    //-------------------------------------------------
+    private void OnHandHoverEnd(Hand hand)
+    {
+        // Hide turret above node
+    }
+
+
+    //-------------------------------------------------
+    // Called every Update() while a Hand is hovering over this object
+    //-------------------------------------------------
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+
+        if (interactable.attachedToHand == null && startingGrabType != GrabTypes.None)
+        {
+            // Call this to continue receiving HandHoverUpdate messages,
+            // and prevent the hand from hovering over anything else
+            hand.HoverLock(interactable);
+
+            // Build turret
+        }
+    }
+}
---
+++ Assets/Shared/Scripts/TestScripts/TestFunction.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFunction : MonoBehaviour {
+	public void Trigger () {
+        Debug.Log("Trigger");
+	}
+}
