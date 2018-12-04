be174c5c94bcfd0187483821dc5f1e6eca33598e
jarett-lee <jarett.lee.pi@gmail.com>
2018-12-03 20:44:13 -0500

    Rename guns and add resonable costs

--- Assets/Shared/Scripts/Shop.cs
+++ Assets/Shared/Scripts/Shop.cs
@@ -1,34 +1,43 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Shop : MonoBehaviour {
+public class Shop : MonoBehaviour
+{
+    public Text turret1Text;
+    public Text turret2Text;
+    public Text turret3Text;
 
-	public TurretBlueprint standardTurret;
-	public TurretBlueprint missileLauncher;
-	public TurretBlueprint laserBeamer;
+    public TurretBlueprint turretBlueprint1;
+    public TurretBlueprint turretBlueprint2;
+    public TurretBlueprint turretBlueprint3;
 
-	BuildManager buildManager;
+    BuildManager buildManager;
 
-	void Start ()
-	{
-		buildManager = BuildManager.instance;
-	}
+    void Start()
+    {
+        buildManager = BuildManager.instance;
 
-	public void SelectTurret1 ()
-	{
-		Debug.Log("Standard Turret Selected");
-		buildManager.SelectTurretToBuild(standardTurret);
-	}
+        turret1Text.text = "Buy turret 1 for $" + turretBlueprint1.cost;
+        turret2Text.text = "Buy turret 2 for $" + turretBlueprint2.cost;
+        turret3Text.text = "Buy turret 3 for $" + turretBlueprint3.cost;
+    }
 
-	public void SelectTurret2 ()
-	{
-		Debug.Log("Missile Launcher Selected");
-		buildManager.SelectTurretToBuild(missileLauncher);
-	}
+    public void SelectTurret1()
+    {
+        Debug.Log("Standard Turret Selected");
+        buildManager.SelectTurretToBuild(turretBlueprint1);
+    }
 
-	public void SelectTurret3 ()
-	{
-		Debug.Log("Laser Beamer Selected");
-		buildManager.SelectTurretToBuild(laserBeamer);
-	}
+    public void SelectTurret2()
+    {
+        Debug.Log("Missile Launcher Selected");
+        buildManager.SelectTurretToBuild(turretBlueprint2);
+    }
+
+    public void SelectTurret3()
+    {
+        Debug.Log("Laser Beamer Selected");
+        buildManager.SelectTurretToBuild(turretBlueprint3);
+    }
 
 }
