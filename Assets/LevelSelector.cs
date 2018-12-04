9736cfd0a082abe606a2754531cc4bad3c661ac0
Wu <wu891@purdue.lcl>
2018-12-03 22:15:59 -0500

    finished levelselect

---
+++ Assets/LevelSelector.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelSelector : MonoBehaviour {
+
+	public void Select (string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
